ab2b88fee88e954f6d50b5adc5ca6b6b12e69b50
aarynverma <18100btcse02667@svvvindore.onmicrosoft.com>
2023-02-13 13:28:10 +0530

    Minor Changes

--- src/Api/APIServices.ts
+++ src/Api/APIServices.ts
@@ -2,7 +2,9 @@ import { dblClick } from "@testing-library/user-event/dist/click";
 import axios from "axios";
 // Use json server :
 //             1. change directory src/Data .
-//             2. start server: json-server --watch db.json --port 3001
+//             2. start server: json-server --watch API_1_Response.json --port 3001
+//             3. start server: json-server --watch API_2_Response.json --port 3002
+
 export const fetchArticleDetails = async () => {
     
   const response = await axios.get(
